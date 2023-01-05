d5f87b34af8ff6bb36b679e50a7af76e2a6e410a
Sean Joseph <sejogoo@gmail.com>
2023-01-04 22:12:01 -0600

    firstNextJsApp: Railway Vercel teamwork test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,14 @@ import styles from "../styles/Home.module.css";
 import { getData, TestResponseData } from "./api/hello";
 import { currentUrl } from "../config";
 const inter = Inter({ subsets: ["latin"] });
+interface WeatherForecast {
+  date: Date;
+  temperatureC: Number;
+  temperatureF: Number;
+  summary: String;
+}
 
-export default function Home(props: { data: TestResponseData }) {
+export default function Home(props: { data: WeatherForecast[] }) {
   console.log(props);
   return (
     <>
@@ -19,105 +25,9 @@ export default function Home(props: { data: TestResponseData }) {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            {props.data.name}...hey! Get started by editing&nbsp;
+            {props.data[0].summary}...hey! Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
@@ -125,10 +35,17 @@ export default function Home(props: { data: TestResponseData }) {
 }
 
 export const getServerSideProps = async () => {
-  console.log(currentUrl);
+  //const data = await getData();
 
-  const data = await getData();
+  const data = await (
+    await fetch(
+      "https://connecttonexttestsol-production.up.railway.app/WeatherForecast"
+    )
+  ).json();
 
+  //Note is fine to call an external api from here using fetch probably
+  ///If that's the case, will need to do more research into the actual purpose of Api Routes...
+  ///Seems to be safe to make this call from the client side but not from getServerSideProps
   return {
     props: {
       data,
